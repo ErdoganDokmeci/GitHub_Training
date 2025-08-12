aabde1871ea968c82e5ced143a312d50dee31bd2
ErdoganDokmeci <erdogan_dokmeci@yahoo.com>
2025-08-12 17:42:11 +0300

    It was 17.42 o'clock

--- main.c
+++ main.c
@@ -12,6 +12,21 @@ int main()
     for(int i = 0; i < 10; i++)
     {
         printf("%d\n", a);
+        printf("1 - Abaci, \
+               2 - Altay, \
+               3 - Demirci,
+               4 - Dökmeci,
+               5 - Duyar,
+               6 - Engin,
+               7 - Gök,
+               8 - Ýnan,
+               9 - Kürekçi,
+               10 - Topuz,
+               11 - Usta,
+               12 - Yavuz,
+               13 - Yelkenci,
+               14 - Yýlmaz
+               ");
     }
     return 0;
 }
