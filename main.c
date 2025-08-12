95cd063222097c06bb3cbaf795d6362e38079a7a
ErdoganDokmeci <erdogan_dokmeci@yahoo.com>
2025-08-12 17:10:31 +0300

    It was 17.10 o'clock

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int a;
 int main()
 {
     printf("Hello world!\n");
     printf("Change favours a prepared mind.\n");
+    printf("Enter a value : ");
+    scanf("%d", &a);
 
-    for(int i = 0; i < 100; i++)
+    for(int i = 0; i < 10; i++)
     {
-
+        printf("%d\n", a);
     }
     return 0;
 }
